fe05cfd0ff4ea1b8a83b9e6c40ab84c1a0b6feb0
AegisX1 <deathwalker1997@gmail.com>
2019-10-12 11:41:20 +0700

    V1.1

--- server.js
+++ server.js
@@ -12,33 +12,67 @@ const database2 = 'sample_airbnb'
 const collection2 = 'listingsAndReviews'
 const client = new MongoClient(MongoURL , {useNewUrlParser :true , useUnifiedTopology : true})
 app.use(express.json())
-app.post('/pokemons',(req,res) => {
-    client.connect((err,client) => {
-        if(err){
-            console.log(err)
-            res.status(500).send({error: err})
-            return 
-        }
-        let db = client.db(database)
-        db.collection(collection).insertOne({ name : 'Raijuu', Type: ' Lightning'} , (err,result) => {
-            res.status(201).send({message : 'Create Pokemon Complete'})
-            return
-        })
+app.post('/pokemons', async(req,res) => {
+    let c = await client.connect().catch((err) => {
+        console.log('Error occurred when try to connect mongo')
+        console.log(err)
+        res.status(500).send({error : err})
+        return
     })
+    let db = c.db(database)
+    let r = await db.collection(collection).insertOne({name: 'Magnet'}).catch((err) => {
+        console.log('Error to insert pokemon')
+        console.log(err)
+        res.status(500).send({error : err})
+        return
+    })
+    res.status(201).send({message : 'Create Pokemon Complete'})
+    // client.connect((err,client) => {
+    //     if(err){
+    //         console.log(err)
+    //         res.status(500).send({error: err})
+    //         return 
+    //     }
+    //     let db = client.db('Pokemondb')
+
+    //     db.collection('pokemons').insertOne({name: 'Magnet'},(err,r) => {
+    //         res.status(201).send({message : 'Create Pokemon Complete'})
+    //         return
+    //     })
+
+    // })
+
+    // client.connect((err,client) => {
+    //     if(err){
+    //         console.log(err)
+    //         res.status(500).send({error: err})
+    //         return 
+    //     }
+    //     let db = client.db(database)
+    //     db.collection(collection).insertOne({ name : 'Raijuu', Type: ' Lightning'} , (err,result) => {
+    //         res.status(201).send({message : 'Create Pokemon Complete'})
+    //         return
+    //     })
+    // })
+    //res.status(200).send({message : 'Finish Too early'})
 })
 
-app.get('/airbnb/list',(req,res) => {
-    client.connect((err,client) => {
-        if(err){
-            console.log(err)
-            res.sendStatus(500).send({error: err})
-            return 
-        }
-        let db = client.db(database2)
-         db.collection(collection2).find({}).limit(10).toArray((err,result) => {
-            res.send(result)
-        })
+app.get('/airbnb/list', async(req,res) => {
+    let c = await client.connect().catch((err) => {
+        console.log('Error occurred when try to connect mongo')
+        console.log(err)
+        res.status(500).send({error : err})
+        return
     })
+    let db = c.db(database2)
+    let r = await db.collection(collection2).find({}).limit(10).toArray().catch((err) => {
+        console.log('Error to insert pokemon')
+        console.log(err)
+        res.status(500).send({error : err})
+        return
+    })
+    res.send(r)
+
 })
 
 app.listen(port, () => console.log(`Pokemon!! API listening on port ${port}!`)) // start server
